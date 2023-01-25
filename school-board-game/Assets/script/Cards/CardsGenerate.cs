df73b3a02167d58eb4a118f16be47da047aa9f7e
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2023-01-25 14:43:36 +0100

    Now the card workds

--- school-board-game/Assets/script/Cards/CardsGenerate.cs
+++ school-board-game/Assets/script/Cards/CardsGenerate.cs
@@ -2,19 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class CardsGenerate : MonoBehaviour
 {
     public GameObject cardPrefab;
     public Vector3 cardPosition;
-    public TextMesh cardLetter;
+    public Vector3 center;
+    public TextMeshProUGUI cardLetter;
 
     private string[] letters = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
     private int[] letterCount = new int[26];
     private int generateCards = 0;
 
+    //Card spacing
+    public Vector3 cardSpacing;
+    private Vector3 currentPosition;
+
     void Start()
     {
+        currentPosition = cardPosition;
         GenerateCards();
     }
 
@@ -32,19 +39,17 @@ public class CardsGenerate : MonoBehaviour
 
             letterCount[letterIndex]++;
 
-            GameObject card = Instantiate(cardPrefab, cardPosition, Quaternion.Euler(0, 90, 0));
+            GameObject card = Instantiate(cardPrefab, currentPosition, Quaternion.identity);
 
+            card.transform.LookAt(center);
+            currentPosition += cardSpacing;
+
+            Quaternion rotation = Quaternion.LookRotation(center - card.transform.position, Vector3.up);
+            card.transform.rotation = rotation;
+            card.transform.Rotate(Vector3.up, 90f);
+
+            Debug.Log(letter);
             cardLetter.text = letter;
-
-            if (cardPosition.x == 0)
-            {
-                cardPosition += new Vector3(3.6f, 0, 0);
-                Debug.Log(cardPosition + new Vector3(3.6f, 0, 0));
-            }
-            else if (cardPosition.z == 0)
-            {
-                cardPosition += new Vector3(0, 0, 3.6f);
-            }
             generateCards++;
         }
     }
