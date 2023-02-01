be71e2371077b1fbdc538006da6d454ac54a9365
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2023-02-01 22:07:49 +0100

    Created the submit form

---
+++ school-board-game/Assets/script/TextSubmit/TextSubmit.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextSubmit : MonoBehaviour
+{
+    public RectTransform submitForm;
+    private float submitFormStatus = 0;
+
+    public void ShowReactangle()
+    {
+        if (submitFormStatus == 0)
+        {
+            submitForm.gameObject.SetActive(true);
+            StartCoroutine(ShowReactangleCoroutine());
+            submitFormStatus = 1;
+        }
+    }
+
+    public void HideReactangle()
+    {
+        if (submitFormStatus == 1)
+        {
+            StartCoroutine(HideReactangleCoroutine());
+            submitFormStatus = 0;
+        }
+    }
+
+    IEnumerator ShowReactangleCoroutine()
+    {
+        float time = 0;
+        Vector2 startPosition = submitForm.anchoredPosition;
+        Vector2 finalPosition = submitForm.anchoredPosition + Vector2.up * 350;
+
+        while (time < 1)
+        {
+            time += Time.deltaTime * 2;
+            submitForm.anchoredPosition = Vector2.Lerp(startPosition, finalPosition, time);
+            yield return null;
+        }
+    }
+
+    IEnumerator HideReactangleCoroutine()
+    {
+        float time = 0;
+        Vector2 startPosition = submitForm.anchoredPosition;
+        Vector2 finalPosition = submitForm.anchoredPosition + Vector2.down * 350;
+
+        while (time < 1)
+        {
+            time += Time.deltaTime * 2;
+            submitForm.anchoredPosition = Vector2.Lerp(startPosition, finalPosition, time);
+            yield return null;
+        }
+    }
+
+}
